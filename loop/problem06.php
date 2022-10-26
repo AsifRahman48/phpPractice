d1e912ba2c4a1b3e18665e39b1e7800c69d9b27e
asif rahman <mdasifrahmanm01@gmail.com>
2022-10-26 12:46:34 +0600

    problemSolving

--- loop/problem06.php
+++ loop/problem06.php
@@ -16,3 +16,21 @@ for ($i=0;$i<strlen($text);$i++)
 echo $count;
 
 ?>
+<!--Write a program which will count the "r" characters in the text "w3resource".-->
+
+<?php
+$text="w3resource";
+$search="r";
+$count="0";
+
+for ($i=0;$i<strlen($text);$i++)
+{
+    if (substr($text,$i,1)==$search)
+    {
+        $count=$count+1;
+    }
+}
+
+echo $count;
+
+?>
